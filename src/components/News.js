54d2a632b338b51095bf6959f5c066ea9acef0b2
Javed Chauhan <chauhanjaved@gmail.com>
2023-07-04 09:30:36 +0530

    Initial Commit

---
+++ src/components/News.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+
+export default class News extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+      totalResults: 0, 
+      pageSize: 20
+    };
+  }
+  async componentDidMount() {
+    let url =
+      `https://newsapi.org/v2/top-headlines?country=in&apiKey=dbe57b028aeb41e285a226a94865f7a7&page=1&pageSize=${this.state.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults});
+  }
+  handlePrevClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=dbe57b028aeb41e285a226a94865f7a7&page=${this.state.page - 1}&pageSize=${this.state.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({ articles: parsedData.articles, page: this.state.page - 1 });
+  };
+  handleNextClick = async () => {
+    if(this.state.page + 1 > Math.ceil(this.state.totalResults / this.state.pageSize)){
+
+    }
+    else{
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=dbe57b028aeb41e285a226a94865f7a7&page=${this.state.page + 1}&pageSize=${this.state.pageSize}`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({ articles: parsedData.articles, page: this.state.page + 1 });
+    }
+    
+  };
+  render() {
+    return (
+      <div className="container my-3">
+        <h2>NewsWave: Top Headlines</h2>
+        <div className="row">
+          {this.state.articles.map((element) => {
+            console.log(element.url);
+            return (
+              <div className="col-md-3" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title : ""}
+                  description={element.description ? element.description : ""}
+                  imageUrl={element.urlToImage ? element.urlToImage : ""}
+                  newsUrl={element.url ? element.url : ""}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
